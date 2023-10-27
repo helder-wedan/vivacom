01ff69e12a73c17dd9dc4d91cb1f9d493669418f
helder-wedan <123248965+helder-wedan@users.noreply.github.com>
2023-10-27 16:33:51 -0300

    Update pesl.py

--- pesl.py
+++ pesl.py
@@ -120,7 +120,7 @@ def arquivos_pesl():
                     base_pesl = base_pesl.groupby([pd.Grouper(key = 'DATA', freq = 'M')]).sum().reset_index().drop(['PLANO','PRESTADOR'],axis=1)
                     base_completa_pesl[str(max(base_pesl.DATA).strftime('%m-%Y'))]=base_pesl
         else:
-        print("A resposta não é uma lista JSON")
+            print("A resposta não é uma lista JSON")
     else:
         print(f"Falha na solicitação HTTP. Código de status: {response.status_code}")
     return base_completa_pesl
